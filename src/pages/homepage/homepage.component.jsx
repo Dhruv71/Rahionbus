ebfc657a4fafd5128deb82b2c7f24398799f274a
kaval1810 <kaval1810@gmail.com>
2020-01-18 13:54:43 +0530

    homepage_scss

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -28,11 +28,10 @@ import './homepage.styles.scss';
  	      <div className='homepage'>
     <div className='item-container'>
     <div className='headtext'>Welcome To GSRTC</div>
-    <div className='subtext'>lorem is the most used dumy text in sublime text.<br/>But todays world is
-        not about the onlysite.It's now<br/>a more than web.I hope this ismake some
-        sense of<br/>online portal.</div>
-      <h2 className='findroutes'>FIND ROUTES</h2>
-     console.log("changing done!");
+    <div className='subtext'>lorem is the most used dumy text in sublime text. But todays world is
+        not about the<br/> onlysite.It's now a more than web.I hope this ismake some
+        sense of online portal.</div>
+    <h2 className='findroutes'>Find Routes</h2>
      <form onSubmit={this.handelSubmit}>
       <CustomInput
        name='source'
@@ -56,25 +55,28 @@ import './homepage.styles.scss';
        type='submit'
         />
      </form>
-     <CardList />
+     <CardList/>
      <div className='about'>
        <h1>About Us</h1>
-       <p>All Tesla vehicles are produced At its factory in Fremont, California,
+
+       <p>All Tesla vehicles are produced at its factory in Fremont, California, where the vast majority of vehicle’s components are also made
+       To achieve our goal of having the safest factories in the world, Tesla
+          is taking a proactive approach to safety, requiring production employees
+       All Tesla vehicles are produced At its factory in Fremont, California,
           where the vast majority of the vehicle’s components are also made.
           To achieve our goal of having the safest factories in the world, Tesla
           is taking a proactive approach to safety, requiring production employees
           to participate in a multi-day training program before ever setting foot
            on the factory floor. From there, Tesla continues to provide on-the-job
-            training and track performance daily so that improvements can be made
-            quickly. The result is that Tesla’s safety rate continues to improve
+          training and track performance daily so that improvements can be made
+           quickly. The result is that Tesla’s safety rate continues to improve
           is taking a proactive approach to safety, requiring production employees
             while Model 3 production ramps.</p>
-     </div>
+    </div>
     </div>
     </div>
 
- );
-
- }
+  );
+};
 }
  export default Homepage;
